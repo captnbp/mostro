b48f0d4b1ca36e2fc61def1bd64a60e18a52f53e
Arnaud Lavrard <arnaud@lavrard.org>
2015-02-16 16:24:26 -0500

    Memcache

--- checks/memcache.sh
+++ checks/memcache.sh
@@ -18,7 +18,7 @@ IFS=$'\r\n'
 
 MEMCACHE_HOST=${MEMCACHE_HOST:-127.0.0.1}
 
-if ! OUTPUT=$(echo -e "stats\nquit" | nc -v "$MEMCACHE_HOST" "$MEMCACHE_PORT" 2>&1)
+if ! OUTPUT=$(echo -e "stats settings\nstats slabs\nstats\nquit" | nc -v "$MEMCACHE_HOST" "$MEMCACHE_PORT" 2>&1)
 then
   echo "error: \"$OUTPUT\""
   exit 254
@@ -32,13 +32,42 @@ do
   KEY=${LINE[1]}
   VALUE=${LINE[2]}
 
-  if [ "${#D[@]}" = "3" ]
+  if [[ \
+    "$KEY" = "curr_connections" || \
+    "$KEY" = "total_connections" || \
+    "$KEY" = "cmd_get" || \
+    "$KEY" = "cmd_set" || \
+    "$KEY" = "cmd_flush" || \
+    "$KEY" = "cmd_touch" || \
+    "$KEY" = "get_hits" || \
+    "$KEY" = "get_misses" || \
+    "$KEY" = "delete_misses" || \
+    "$KEY" = "delete_hits" || \
+    "$KEY" = "incr_misses" || \
+    "$KEY" = "incr_hits" || \
+    "$KEY" = "decr_misses" || \
+    "$KEY" = "decr_hits" || \
+    "$KEY" = "cas_misses" || \
+    "$KEY" = "cas_hits" || \
+    "$KEY" = "cas_badval" || \
+    "$KEY" = "touch_hits" || \
+    "$KEY" = "touch_misses" || \
+    "$KEY" = "bytes_read" || \
+    "$KEY" = "bytes_written" || \
+    "$KEY" = "limit_maxbytes" || \
+    "$KEY" = "bytes" || \
+    "$KEY" = "curr_items" || \
+    "$KEY" = "total_items" || \
+    "$KEY" = "evictions" || \
+    "$KEY" = "reclaimed" || \
+    "$KEY" = "maxconns" || \
+    "$KEY" = "active_slabs" || \
+    "$KEY" = "total_malloced"
+  ]]
   then
-    DATA+=(["${D[1]}"]="${D[2]}")
-  fi
-
-  if [[ "$KEY" = "cmd_get" || "$KEY" = "cmd_set" || "$KEY" = "get_hits" || "$KEY" = "evictions" ]]
-  then
-    echo "${KEY}: ${VALUE}"
+    if [[ "$VALUE" =~ ^[0-9]+$ ]]
+    then
+      echo "${KEY}: ${VALUE}"
+    fi
   fi
 done
