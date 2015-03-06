d225c6347a39a0bc04f7b6d16cb73b72cb33d1ec
Arnaud Lavrard <arnaud@lavrard.org>
2015-03-06 13:26:31 -0500

    Report NTP peer status as a boolean

--- checks/ntp_offset.sh
+++ checks/ntp_offset.sh
@@ -3,20 +3,26 @@
 echo "---"
 echo "service: ntp_offset"
 
-PEERS=$(ntpq -nc peers 2>&1)
+VARIABLES=$(ntpq -nc readvar 2>&1)
 
-if [[ "$?" != "0" || $PEERS =~ ^ntpq ]]
+if [[ "$?" != "0" || $VARIABLES =~ ^ntpq ]]
 then
-  echo "error: \"$PEERS\""
+  echo "error: \"$VARIABLES\""
   exit 254
 fi
 
-OFFSET=$(echo "$PEERS" | grep '^\*' | awk '{ print $9 }')
+OFFSET=0
+CONNECTED=0
 
-if [ -z "$OFFSET" ]
+if [[ "$VARIABLES" =~ offset=([-\.0-9]+) ]]
 then
-  echo "error: No active peer."
-  exit 254
+  OFFSET=${BASH_REMATCH[1]}
 fi
 
+if [[ "$VARIABLES" =~ refid=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} ]]
+then
+  CONNECTED=1
+fi
+
+echo "connected: $CONNECTED"
 echo "offset: $OFFSET"
