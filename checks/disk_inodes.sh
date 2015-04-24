234752fcc1c7dfcb296a53866246579549a24d2e
Arnaud Lavrard <arnaud@mostro.com>
2015-04-24 13:39:05 -0400

    Some filesystems don't use inodes

--- checks/disk_inodes.sh
+++ checks/disk_inodes.sh
@@ -19,5 +19,11 @@ fi
 
 DATA=( $DATA )
 
+if [[ "${DATA[0]}" = "0" ]]
+then
+  echo "error: The filesystem doesn't use inodes."
+  exit 254
+fi
+
 echo "free_inodes: ${DATA[1]}"
 echo "total_inodes: ${DATA[0]}"
