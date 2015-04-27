57f884628b9b5e93ba6d259ac5af17accefaefa0
Arnaud Lavrard <arnaud@lavrard.org>
2015-04-27 16:15:07 -0400

    Disk IO latency

--- checks/disk_io.sh
+++ checks/disk_io.sh
@@ -11,7 +11,7 @@ fi
 
 echo "argument: $1"
 
-if ! DATA=$(cat "/sys/block/$1/stat" 2>&1) || ! SECTORSIZE=$(cat "/sys/block/$1/queue/hw_sector_size" 2>&1)
+if ! DATA=$(cat "/sys/block/$1/stat" 2>&1)
 then
   echo "error: Couldn't get data for $1"
   exit 254
@@ -20,7 +20,9 @@ fi
 DATA=( $DATA )
 
 echo "reads: ${DATA[0]}"
-echo "writes: ${DATA[4]}"
 echo "sector_reads: ${DATA[2]}"
+echo "ticks_reads: ${DATA[3]}"
+echo "writes: ${DATA[4]}"
 echo "sector_writes: ${DATA[6]}"
-echo "sector_size: $SECTORSIZE"
+echo "ticks_writes: ${DATA[7]}"
+echo "sector_size: 512"
